382e1baf44b9f38a64dbbc51ce9529c49b0993f9
thameemk612 <thameemk612@gmail.com>
2018-11-24 21:52:07 +0530

    config

--- dynamic/application/config/config.php
+++ dynamic/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/thameem.me';
+$config['base_url'] = 'http://localhost/thameemk612.github.io/dynamic/';
 
 /*
 |--------------------------------------------------------------------------
